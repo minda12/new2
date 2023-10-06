6e0326243d1fffd9859c6b8aaeebab17a94c3b56
minda12 <emcsmarty@gmail.com>
2023-10-06 19:11:02 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,27 @@
+//console.dir(document);
+console.log(document.domain)
+console.log(document.URL)
+console.log(document.title)
+
+//document.title= 123
+console.log(document.doctype)
+console.log(document.head)
+console.log(document.body)
+
+//console.log(document.all)
+//console.log(document.all[10])
+//document.all[10].textContent ='hello'
+console.log(document.forms[0])
+console.log(document.images)
+
+s
+//console.log(document.getElementById("header-title"))
+var headerTitle= document.getElementById("header-title")
+var header= document.getElementById("main-header")
+//console.log(headerTitle)
+//headerTitle.textContent='bybye'
+//headerTitle.innertext='bybye'
+//headerTitle.innerHTML='<h3>Heelllo</h3>'
+
+header.style.borderBottom = "solid 10px #000"
+
