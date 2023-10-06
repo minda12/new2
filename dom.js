98ff0ddcc92613c64b87ea52b00fe02876cfefa2
minda12 <emcsmarty@gmail.com>
2023-10-06 21:04:04 +0530

    Make the 3 rd element in the list have green background color Make all the elements in the list have bold color font

--- dom.js
+++ dom.js
@@ -27,9 +27,22 @@ header.style.borderBottom ="solid 20px #000"
 
 var items = document.getElementsByClassName("list-group-item")
 console.log(items[2])
-items[1].textContent="Hello"
-items[1].style.fontWeight="bold"
-items[1].style.backgroundColor="yellow"
+
+//items[1].textContent="Hello"
+//items[1].style.fontWeight="bold"
+//items[1].style.backgroundColor="yellow"
 var add =document.querySelector('.title')
+
 add.style.fontWeight='bold'
 add.style.color="green"
+
+
+items[2].style.backgroundColor="green"
+
+for(var i =0;i<items.length;i++)
+{
+
+items[i].style.fontWeight="bold"
+items[i].style.color="blue"
+
+}
