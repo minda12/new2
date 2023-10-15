5b0c68d502d33adebe7e327b59ada7d6ed0cae02
minda12 <emcsmarty@gmail.com>
2023-10-15 14:28:30 +0530

    On clicking the delete button we should be able to remove the newly created li tag Now add an edit button next to the delete icon.[Dont have to build the Edit  functionality, just add the button]

---
+++ main.js
@@ -0,0 +1,84 @@
+var form =document.getElementById('addForm')
+
+var itemList=document.getElementById('items')
+
+var filter=document.getElementById('filter')
+
+//form submit event
+
+//eventlistner for adding
+form.addEventListener('submit',addItem)
+//eventlistner for deleting
+itemList.addEventListener('click',delItem)
+
+// eventlistner forFILTER ITEMS 
+filter.addEventListener('keyup', filterItems)
+
+//function to Add item 
+function addItem(e){
+
+    e.preventDefault()
+    
+    var newItem =document.getElementById('item').value
+
+    //create new ki element 
+
+    var li = document.createElement('li')
+
+    //add class 
+    li.className = 'list-group-item'
+    console.log(li)
+
+    //add text node with input value 
+
+    li.appendChild(document.createTextNode(newItem))
+
+    //create dlete button
+
+    var delbutton =document.createElement('button')
+
+    //delbtnclasses
+    delbutton.className='btn btn-danger btn-sm float-right delete'
+    delbutton.appendChild(document.createTextNode('x'))
+    li.appendChild(delbutton)
+
+    itemList.appendChild(li)
+}
+
+
+   function delItem(e){
+if(e.target.classList.contains('delete'))
+{
+    if (confirm('Are you Sure'))
+    {
+        var li = e.target.parentElement
+        itemList.removeChild(li)
+
+    }
+}
+
+}
+
+function filterItems(e)
+{
+    //convet to lowercase
+    var text =e.target.value.toLowerCase()
+    
+
+    var items = itemList.getElementsByTagName('li')
+  
+
+    Array.from(items).forEach(function(item)
+    {
+        var ItemName = item.firstChild.textContent
+        if(ItemName.toLowerCase().indexOf(text) != -1)
+        {
+            item.style.display='block'
+
+        }
+        else{
+            item.style.display='none'
+
+        }
+    })
+}
